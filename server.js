2954c673edaa3f7502a7ec92c8fabee841baa4a4
Chris Wilson <aboredguy52@gmail.com>
2015-07-02 14:25:22 -0400

    Added GET and POST processing
    - GET and POST variables are now available in workers via
    - - get.varName
    - - post.varName
    
    - Worker control file has been transformed into a function that can be instantiated
    - Added some test cases to the index.html file
    - Worker files no longer need to use this.apply() to assign variables to markers. They can now simply use apply()
    - Server flow has been modified to allow GET and POST processing
    - Request object has been made available to worker files

--- server.js
+++ server.js
@@ -4,9 +4,27 @@ var 	http = 		require('http'),
 	strftime = 	require('strftime'),
 	fs = 		require('fs'),
 	path = 		require('path'),
+	qs = 		require('querystring');
 	file = 		require('./lib/contentType'),
 	worker = 	require('./lib/worker')
 ;
+
+// Global config
+var listenPort = 1500;
+
+var sendResponse = function(request,response,incoming,data,workman){	
+	// Set the worker loose on the template
+	data = workman.doWork();
+	
+	// Determine the content type of the file we are responding with
+	var contentType = file.contentType(incoming.pathname);
+	response.writeHeader(200,{"Content-Type":contentType});
+
+	// Send the file contents to the response
+	response.write(data);
+	response.end();
+	console.log('Responded to request from '+request.connection.remoteAddress+' with /templates'+incoming.pathname);			
+}
 
 var httpCallback = function(request,response){
 	// Let's gather and log info on the requestor
@@ -30,20 +48,38 @@ var httpCallback = function(request,response){
 			return;
 		}
 
-		var workman = worker.worker;
-		workman.build(data,incoming.pathname);
-		data = workman.doWork();
+		// Create the worker object and set it's mandatory data
+		var workman = new worker.worker(data,incoming.pathname,request);
 
-		// Determine the content type of the file we are responding with
-		var contentType = file.contentType(incoming.pathname);
-		response.writeHeader(200,{"Content-Type":contentType});
+		// Find any get variables (POST or not - it's always possible we get a combo request)
+		workman.setGet(url.parse(request.url,true).query);
 
-		// Send the file contents to the response
-		response.write(data);
-		response.end();
-		console.log('Responded to request from '+request.connection.remoteAddress+' with /templates'+incoming.pathname);
+		// Was this a POST request?
+		if(request.method === 'POST'){
+			var postBody = '';
+			request.on('data', function(data){
+				// Make sure nobody tried to flood the RAM
+				postBody += data;
+				if(postBody.length > 1e6){
+					console.log('Request from '+request.connection.remoteAddress+' was an attempt to flood RAM. Destroying connection.');
+					request.connection.destroy();
+					response.end('Too much data submitted!');
+					return;
+				}
+			});
+	
+			// Make sure the request processing is done before calling the worker
+			request.on('end',function(){
+				workman.setPost(qs.parse(postBody));
+				sendResponse(request,response,incoming,data,workman);
+			});
+			// Return here so we don't accidentally try to write to the response after closing it.
+			return;
+		}
+		sendResponse(request,response,incoming,data,workman);
 	});
 };
 
 var httpServer = http.createServer(httpCallback);
-httpServer.listen(3000);
+httpServer.listen(listenPort);
+console.log('Server running on port '+listenPort);
--- workers/index.html.js
+++ workers/index.html.js
@@ -1,2 +1,10 @@
 // Example to set data for a marker
-this.apply('exampleMarker','This text was filled by a worker!');
+apply('exampleMarker','This text was filled by a worker!');
+
+// Show off the GET and POST data
+apply('getData',JSON.stringify(get));
+apply('postData',JSON.stringify(post));
+
+// Fill in the text boxes
+apply('testPost1',post.testPost1);
+apply('testPost2',post.testPost2);
