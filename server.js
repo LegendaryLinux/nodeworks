cc00b5cded08535567edc1401a95ac18e97daa5e
LegendaryLinux <aboredguy52@gmail.com>
2015-08-14 19:26:28 -0400

    A couple minor changes/fixes
    - Included a new mimetype detector
    - Removed an unused dependency
    - Tweaked the syntax a bit
    - Added a couple comments

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-// Dependencies
-var 	http = 		require('http'),
+var	// Native Dependencies
+	http = 		require('http'),
 	url = 		require('url'),
-	strftime = 	require('strftime'),
 	fs = 		require('fs'),
-	path = 		require('path'),
-	qs = 		require('querystring');
-	file = 		require('./lib/contentType'),
-	worker = 	require('./lib/worker')
+	qs = 		require('querystring'),
+
+	// Local Dependencies
+	worker = 	require('./lib/worker'),
+
+	// NPM Dependencies
+	strftime = 	require('strftime'),
+	mime =		require('mime')
 ;
 
 // Global config
@@ -17,28 +20,28 @@ var sendResponse = function(request,response,incoming,data,workman){
 	data = workman.doWork();
 	
 	// Determine the content type of the file we are responding with
-	var contentType = file.contentType(incoming.pathname);
+	var contentType = mime.lookup(incoming.pathname);
 	response.writeHeader(200,{"Content-Type":contentType});
 
 	// Send the file contents to the response
 	response.write(data);
 	response.end();
 	console.log('Responded to request from '+request.connection.remoteAddress+' with /templates'+incoming.pathname);			
-}
+};
 
 var httpCallback = function(request,response){
 	// Let's gather and log info on the requestor
 	var incoming = url.parse(request.url);
-	var currentDate = new Date();
 	console.log("Received request from "+request.connection.remoteAddress+' for '+incoming.pathname+' at '+strftime('%F %T',new Date()));
 
 	// People requesting / usually want the index
 	if(incoming.pathname === '/') incoming.pathname = '/index.html';
 
 	// Try to serve the requested template
-	var fileContents = fs.readFile(__dirname+'/templates'+incoming.pathname,function(err,data){
+	fs.readFile(__dirname+'/templates'+incoming.pathname,function(err,data){
 		if(err){
-			var errorPage = fs.readFile(__dirname+'/templates/404.html',function(subErr,subData){
+			// Error page handling
+			fs.readFile(__dirname+'/templates/404.html',function(subErr,subData){
 				if(subErr) return;
 				response.writeHeader(404,{"Content-Type":"text/html"});
 				response.write(subData);
@@ -64,7 +67,7 @@ var httpCallback = function(request,response){
 					console.log('Request from '+request.connection.remoteAddress+' was an attempt to flood RAM. Destroying connection.');
 					request.connection.destroy();
 					response.end('Too much data submitted!');
-					return;
+					return null;
 				}
 			});
 	
